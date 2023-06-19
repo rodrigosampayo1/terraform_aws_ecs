61d79be0b21b13600658307dfac325d6107254b6
rodrigo sampayo <rodrigosampayo1@gmail.com>
2023-06-19 10:16:05 -0300

    My first commit

---
+++ Deployments/Global/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+terraform {
+    backend "s3"{
+        bucket = "terraform-state-mgibson"
+        key    = "Deployments/global/global.tfstate"
+        region = "us-east-1"
+        encrypt = true
+    }
+}
+
+module "s3" {
+    source = "../../Modules/s3"
+    bucket_name = "mgibson-us-east-2-elb-access-logs"
+    environment = "prod"
+    enable_encryption = true
+    enable_versioning = false
+    account_id = "+"
+}
